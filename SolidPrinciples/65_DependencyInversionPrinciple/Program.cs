4951b649b7f313f2fa621fc857190502c7f255f3
matheusluis-dev <matheusluis.dev@proton.me>
2023-05-27 18:18:41 -0300

    Page 65: Dependency Inversion Principle

--- SolidPrinciples/65_DependencyInversionPrinciple/Program.cs
+++ SolidPrinciples/65_DependencyInversionPrinciple/Program.cs
@@ -1,2 +1,84 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+// Interface Segregation Principle
+// Page 65
+//
+// ==================================================
+//
+// The Dependency Inversion Principle (DIP) is a principle in software development that promotes the 
+// decoupling of modules or classes by inverting the traditional dependency flow. It suggests that high-level 
+// modules should not depend on low-level modules; both should depend on abstractions. This principle encourages 
+// the use of interfaces or abstract classes to define contracts and dependencies, allowing for flexibility, extensibility,
+// and easier testing. By applying DIP, changes in low-level modules do not directly impact high-level modules, 
+// promoting modular and loosely coupled systems. It enables the interchangeability of components and facilitates the introduction 
+// of new implementations without modifying existing code.
+
+// High level
+interface IDatabase
+{
+    void Select(DateTime dateTime);
+    void Insert();
+    void Update();
+    void Delete();
+}
+
+class BudgetReport
+{
+    public IDatabase Database { get; set; }
+
+    void Open(DateTime dateTime)
+    {
+        Database.Select(dateTime);
+    }
+
+    void Save()
+    {
+        Database.Update();
+    }
+}
+
+// Low level
+class MySql : IDatabase
+{
+    void Select(DateTime dateTime)
+    {
+
+    }
+
+    void Insert()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Delete()
+    {
+
+    }
+}
+
+class MongoDb : IDatabase
+{
+    void Select(DateTime dateTime)
+    {
+
+    }
+
+    void Insert()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void Delete()
+    {
+
+    }
+}
